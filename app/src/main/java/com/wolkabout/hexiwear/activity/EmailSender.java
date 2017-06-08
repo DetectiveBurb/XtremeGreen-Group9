80f1547d903b8b07dcef88bbe4e11b5acbce65e7
Ark <digitalshadow2.0@gmail.com>
2017-06-08 12:19:26 -0300

    Email Sender Version 1. Basic framework.

---
+++ app/src/main/java/com/wolkabout/hexiwear/activity/EmailSender.java
@@ -0,0 +1,14 @@
+package com.wolkabout.hexiwear.activity;
+
+import android.util.Patterns;
+
+import java.util.regex.Pattern;
+
+/**
+ * Created by ARk on 2017-06-08.
+ */
+
+public class EmailSender {
+
+    Pattern emailPattern = Patterns.EMAIL_ADDRESS;
+}
